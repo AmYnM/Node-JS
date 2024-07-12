350c9a3fd5f1cae28ecc0a8c2b9439bd13ef4868
AmYnM <amyn23ayub@gmail.com>
2024-07-12 10:48:00 +0530

    Updated Add product,shop, added handlebars and added main layout

--- routes/shop.js
+++ routes/shop.js
@@ -12,7 +12,15 @@ router.get('/', (req, res, next) => {
 //   console.log('shop.js', adminData.products);
 //   res.sendFile(path.join(rootDir, 'views', 'shop.html'));
 const products = adminData.products;
-res.render('shop', { prods: products, pageTitle: 'Shop', path: '/' });
+res.render('shop', {
+     prods: products, 
+     pageTitle: 'Shop', 
+     path: '/', 
+     hasProduct: products.length > 0,
+     activeShop: true,
+     productCSS: true,
+     layout: false
+    }); //Adding new key value pair "hasProduct" that have the key to check true or false
 });
 
 module.exports = router;
