2e434e7cc1b2611ca2c7b8703dbd606dbb3ea876
AmYnM <amyn23ayub@gmail.com>
2024-07-18 21:07:47 +0530

    Updated

--- models/product.js
+++ models/product.js
@@ -1,15 +1,90 @@
-const products = [];
+// // const products = []; //saving in array
+// // Save using file
+// const fs = require('fs');
+// const path = require('path');
+
+// module.exports = class Product {
+//   constructor(t) {
+//     this.title = t;
+//   }
+    
+//   save() {
+//     // products.push(this);    // this refer to the object created based on the class 
+//     const p = path.join(
+//       path.dirname(require.main.filename), 
+//       'data', 
+//       'products.json'
+//     );
+//     fs.readFile(p, (err, fileContent) => {
+//       let products = [];
+//       if (!err) {
+//         products = JSON.parse(fileContent); // takes incoming json and give back a javascript array or object or anything else
+//        }
+//       products.push(this);
+//       fs.writeFile(p, JSON.stringify(products), err => {
+//         console.log(err);
+//       });
+//     });
+//   }
+//   // retrive all products from array
+//   static fetchAll(cb) {
+//     const p = path.join(
+//       path.dirname(require.main.filename), 
+//       'data', 
+//       'products.json'
+//     );
+//     fs.readFile(p, (err, fileContent) => {
+//       if (err) {
+//         cb([]);
+//       }
+//      cb(JSON.parse(fileContent));
+//     });     // static(keyword) is used for not creating new object with some new keyword
+//     return products;
+//   } 
+// }
+
+const fs = require('fs');
+const path = require('path');
+
+const getProductsFromFile = () => {
+
+}
 
 module.exports = class Product {
-    constructor(t) {
-        this.title = t; // the title of the product is t
-    }
-    save() {
-        products.push(this);    // this refer to the object created based on the class 
-    }
+  constructor(t) {
+    this.title = t;
+  }
 
-    // retrive all products from array
-    static fetchAll() {     // static(keyword) is used for not creating new object with some new keyword
-        return products;
-    } 
+  save() {
+    const p = path.join(
+      path.dirname(require.main.filename), 
+      'data', 
+      'products.json'
+    );
+    fs.readFile(p, (err, fileContent) => {
+      let products = [];
+      if (!err) {
+        products = JSON.parse(fileContent);
+     }
+      products.push(this);
+      fs.writeFile(p, JSON.stringify(products), err => {
+        console.log(err);
+      });
+    });
+  }
+
+  static fetchAll(cb) {
+    const p = path.join(
+      path.dirname(require.main.filename), 
+      'data', 
+      'products.json'
+    );
+    fs.readFile(p, (err, fileContent) => {
+      if (err) {
+        cb([]);
+      }
+     cb(JSON.parse(fileContent));
+    //  return products;
+    });
+   } 
 }
