9f03da25add06e1d13b7a24333d1c640f1df5ecb
AmYnM <amyn23ayub@gmail.com>
2024-07-26 10:04:41 +0530

    "Refactored controllers, routes, and views for admin and shop functionality; added new shop views and updated navigation"

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,5 +1,3 @@
-// no product array related logic left
-// create instead a class
 const Product = require('../models/product'); 
 
 exports.getAddProduct = (req, res, next) => {   // export with the name of your wish
@@ -19,21 +17,14 @@ exports.postProduct = (req, res, next) => {
     const product = new Product(req.body.title);        // Here the title is from the input type and input name in add-product ejs file
     product.save(); // this will use the save method used in model file
     res.redirect('/');
-  };
+};
 
-exports.getproducts = (req, res, next) => {
-    //   console.log('shop.js', adminData.products);
-    //   res.sendFile(path.join(rootDir, 'views', 'shop.html'))'
+exports.getProducts = (req, res, next) => {
     Product.fetchAll((products) => {
-      res.render('shop/product-list',{
-        prods: products, 
-        pageTitle: 'Shop', 
-        path: '/', 
-        hasProduct: products.length > 0,
-        activeShop: true,
-        productCSS: true,
-        layout: false
-       }); //Adding new key value pair "hasProduct" that have the key to check true or false
-    });
-
+        res.render('admin/products',{
+          prods: products, 
+          pageTitle: 'Admin Products', 
+          path: '/admin/products'
+         });
+      });
 };
